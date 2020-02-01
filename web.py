3f60f9795732b97108924e9eed69a37328953762
Vasilisa <31583978+Ebanchous@users.noreply.github.com>
2020-02-01 13:54:39 +0300

    minor updates

--- web.py
+++ web.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from datetime import datetime
 import csv
+import time
 
 
 
@@ -33,67 +34,73 @@ df = df.iloc[ : , 2].values.tolist()
 count = 0
 today = datetime.now().strftime("%d-%m-%Y")
 start_time = datetime.now()
+execution_limit = time.time() + 10	
 for url in df:
-	current_time = datetime.now()
-	time_difference = (current_time - start_time).total_seconds()
-	print('current site is number', count)
-	print('the name of ste site is:', url)
-	try:
-		speed = '{:.2f}'.format(count / time_difference)
-		print('average speed per second = ', speed)
+	try:	
+		current_time = datetime.now()
+		time_difference = (current_time - start_time).total_seconds()
+		print('current site is number', count)
+		print('the name of ste site is:', url)		
+		try:
+			speed = '{:.2f}'.format(count / time_difference)
+			print('average speed per second = ', speed)
+		except Exception as err:
+	#		print('cannot do it because of ', err.args)
+			pass
+		count += 1
+		url_full = str('http://www.') + str(url)
+	#		print('trying url:', url_full)
+		try:
+			response = requests.get(url_full, timeout = 1)
+			response = response.text
+		#except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
+		except Exception as err:
+			response = ''
+	#		print('cannot do it because of ', err.args)
+	#		print(f'Attempt failed, because of {err.args}')
+			# ignore pages with errors and continue with next url
+			pass
+		try:
+			soup = BeautifulSoup(response, 'lxml')
+		except Exception as err:
+	#		print('cannot do it because of ', err.args)
+			soup = ''
+			# ignore pages with errors and continue with next url
+			pass
+		
+		try:
+			header = soup.find('head').find('title').text.strip()
+		except Exception as err:
+	#		print('cannot do it because of ', err.args)
+			header = '-'
+			# ignore pages with errors and continue with next url
+			pass
+
+		try:
+			emails = set(re.findall(r'\w+@\w+\.{1}\w+', 
+				response, re.I)) # re.I: (ignore case)
+		except Exception as err:
+	#		print('cannot do it because of ', err.args)
+	#		print(f'Attempt failed, because of {err.args}')
+			emails = '-'
+			pass
+		mail = 1
+		for email in emails:
+			print ('site:', count - 1, 'e-mail:', mail)
+			mail += 1
+	#		print('header:', header)
+	#		print('email:', email)
+			now = datetime.now()
+			current_time = now. strftime("%d/%m/%Y - %H:%M:%S")	
+			data = {
+				'url': url_full,
+				'email': email,
+				'header': header,
+				'time_of_completion':current_time,
+				}
+			print('writting csv')
+			write_csv(data, today)
+
 	except Exception as err:
-#		print('cannot do it because of ', err.args)
+		print('cannot do it because of ', err.args)
 		pass
-	count += 1
-	url_full = str('http://www.') + str(url)
-#	print('trying url:', url_full)
-	try:
-		response = requests.get(url_full, timeout = 1)
-		response = response.text
-	#except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
-	except Exception as err:
-		response = ''
-#		print('cannot do it because of ', err.args)
-#		print(f'Attempt failed, because of {err.args}')
-		# ignore pages with errors and continue with next url
-		continue
-	try:
-		soup = BeautifulSoup(response, 'lxml')
-	except Exception as err:
-#		print('cannot do it because of ', err.args)
-		soup = ''
-		# ignore pages with errors and continue with next url
-		continue
-	
-	try:
-		header = soup.find('head').find('title').text.strip()
-	except Exception as err:
-#		print('cannot do it because of ', err.args)
-		header = '-'
-		# ignore pages with errors and continue with next url
-		continue
-
-	try:
-		emails = set(re.findall(r'\w+@\w+\.{1}\w+', 
-			response, re.I)) # re.I: (ignore case)
-	except Exception as err:
-#		print('cannot do it because of ', err.args)
-#		print(f'Attempt failed, because of {err.args}')
-		emails = '-'
-	mail = 1
-	for email in emails:
-		print ('site:', count - 1, 'e-mail:', mail)
-		mail += 1
-#		print('header:', header)
-#		print('email:', email)
-		now = datetime.now()
-		current_time = now. strftime("%d/%m/%Y - %H:%M:%S")	
-		data = {
-			'url': url_full,
-			'email': email,
-			'header': header,
-			'time_of_completion':current_time,
-			}
-		print('writting csv')
-		write_csv(data, today)
-
